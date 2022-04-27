d03db0facb17f0fcc0d3ea29443edbb412fb3df7
Choseoyoung <seoyoung0000@naver.com>
2022-04-28 01:11:43 +0900

    Initial commit

---
+++ pybo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+
+# Create your views here.
+def index(request):
+    '''
+    pybo 목록 출력
+    '''
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list':question_list}
+    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
